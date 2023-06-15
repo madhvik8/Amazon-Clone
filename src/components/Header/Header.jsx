7f15e587696cbae978df638548e6f8e19cc37bcd
madhvik8 <madhvikrana@gmail.com>
2023-06-15 12:52:36 +0530

    mad

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,12 +23,12 @@ function Header() {
       <div className="header__nav">
         
           <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest </span>
+            <span className="header__optionLineOne">Hello Guest madhviii</span>
             <span className="header__optionLineTwo">Sign In</span>
           </div>
         
         <div className="header__option">
-            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineOne">Returnss</span>
             <span className="header__optionLineTwo">& Orders</span>
           </div>
         
