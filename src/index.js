1bd58aaa05d165201e461e7d3db6fba17ad6f00c
manish1802 <mk8418954@gmail.com>
2023-06-15 11:45:08 +0530

    commit5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './components/StateProvider/Stateprovider';
 import reducer, { initialState } from './components/StateProvider/Reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
