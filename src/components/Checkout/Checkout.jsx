47919cad9b6e861b25da30f8ac91ac71d971499d
Madhvi Kunwar Rana <madhvikrana@gmail.com>
2023-06-20 10:27:03 +0530

    commit18

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import "./Checkout.css";
 import checkimg from '../../Assets/img12.jpg'
 import Subtotal from './Subtotal';
+import { useStateValue } from '../StateProvider/Stateprovider';
+import CheckoutProduct from './CheckoutProduct';
  
 
 function Checkout() {
+  const[{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className='checkout__left'>
@@ -12,6 +16,17 @@ function Checkout() {
  
         <div>
           <h2 className='checkout__title'>Your Shopping Cart</h2>
+
+          {basket.map(item => {
+          <CheckoutProduct
+          id={item.id}
+          title={item.title}
+          image={image.title}
+          price={item.price}
+          rating={item.rating}
+          />
+        })}
+
           {/* CheckoutProduct */}
           {/* CheckoutProduct */}
           {/* CheckoutProduct */}
@@ -19,6 +34,7 @@ function Checkout() {
           {/* CheckoutProduct */}
            
         </div>
+      
          </div>
 
          <div className='checkout__right'>
--- src/components/Checkout/CheckoutProduct.jsx
+++ src/components/Checkout/CheckoutProduct.jsx
@@ -10,10 +10,20 @@ function CheckoutProduct({ id, image, title, price, rating }) {
 
         <div className='checkoutProduct__info'>
             <p className='checkoutProduct__title'>{title}</p>
-            
+            <p className='checkoutProduct__price'>
+              <small>₹</small>
+              <strong>{price}</strong>
+             </p>
+             <div className='checkoutProduct__rating'>
+              {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>🌟</p>
+              ))}
+             </div>
+             <button>Remove From Basket</button>
             </div> 
-      
-    </div>
+          </div>
   )
 }
 
